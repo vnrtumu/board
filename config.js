b551661cd15e5ffa7f044840586a07d7ab8441d2
venkat reddy <vnr.tumu@gmail.com>
2020-10-13 22:25:20 +0530

    finial fix

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 export default {
-  API_URL: 'http://www.boardpointers.ml/api',
-  // API_URL: 'http://192.168.1.109/boardpointers/public/api',
-  // API_URL: 'http://192.168.1.105/boardpointers/public/api',
-  // API_URL: 'http://192.168.1.239/boardpointers/public/api',
+  API_URL: 'https://boardpointers.com/api',
 };
